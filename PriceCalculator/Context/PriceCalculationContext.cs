b9348b6398fe3a2b122f1d41fc5ca107f34cbfc2
Zoks <z@mail.com>
2020-01-11 15:46:25 +0100

    Added more tests

---
+++ PriceCalculator/Context/PriceCalculationContext.cs
@@ -0,0 +1,30 @@
+﻿using PriceCalculator.Builder;
+using PriceCalculator.Interfaces;
+using PriceCalculator.PriceCalculationStrategies;
+using PriceCalculator.Reporters;
+using System;
+
+namespace PriceCalculator.Context
+{
+	public class PriceCalculationContext
+	{
+		private IPriceCalculation _calculator = new SimplePriceCalculator();
+		private PriceModifiersBuilder _priceModifiers;
+		public PriceCalculationContext() { }
+
+		public PriceCalculationContext(PriceModifiersBuilder priceModifiers)
+		{
+			_priceModifiers = priceModifiers ?? throw new ArgumentNullException(nameof(priceModifiers));
+		}
+
+		public void SetModifiers(PriceModifiersBuilder priceModifiers)
+		{
+			_priceModifiers = priceModifiers ?? throw new ArgumentNullException(nameof(priceModifiers));
+		}
+
+		public void CalculateAndReportPrice(IProduct product)
+		{
+			ConsoleReporter.PrintResult(_calculator.GetPriceResultForProduct(product, _priceModifiers));
+		}
+	}
+}
---
+++ PriceCalculator/Models/Cost.cs
@@ -0,0 +1,18 @@
+﻿using PriceCalculator.Interfaces;
+
+namespace PriceCalculator.Models
+{
+	public class Cost
+	{
+		public Cost(decimal tax, decimal discounts, decimal expenses)
+		{
+			Tax = tax;
+			Discounts = discounts;
+			Expenses = expenses;
+		}
+
+		public decimal Tax { get; }
+		public decimal Discounts { get; }
+		public decimal Expenses { get; }
+	}
+}
